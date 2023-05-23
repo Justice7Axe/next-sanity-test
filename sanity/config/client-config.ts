c44e87d6a17a614c0f1cd27772534235853a568c
Justice7Axe <justice7axe@gmail.com>
2023-05-23 23:27:14 +0900

    first

--- sanity/config/client-config.ts
+++ sanity/config/client-config.ts
@@ -1,7 +1,9 @@
 const config = {
-  projectId: "txd29ab2",
-  dataset: "production",
-  apiVersion: "2023-03-09",
-}
+  // projectId: "txd29ab2",
+  projectId: 'bbglbjvn',
+
+  dataset: 'production',
+  apiVersion: '2023-03-09',
+};
 
 export default config;
